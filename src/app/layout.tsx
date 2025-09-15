b921994ace47ad3d77f47c2509073921e91576d8
obaodelana <obaloluwaode@gmail.com>
2025-09-15 10:18:07 -0600

    Moved header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Montserrat, PT_Serif } from "next/font/google";
-import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const montserrat = Montserrat({
 	variable: "--font-mont",
@@ -26,10 +29,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${pt_serif.variable} ${montserrat.variable} antialiased`}
-      >
-        {children}
+      <body className={`${pt_serif.variable} ${montserrat.variable} antialiased`}>
+		<div className="min-h-screen flex flex-col font-[family-name:var(--font-mont)]">
+			<header className="flex flex-row justify-between items-center px-3 py-2">
+				<div className="flex flex-row items-center justify-between gap-2.5">
+					<Image
+						src="/Logo.png"
+						alt="WCIE Logo"
+						width={40}
+						height={40}/>
+					<p className="text-2xl font-[family-name:var(--font-pt-serif)]">WCIE Transport</p>
+				</div>
+				<Link href="tel:+17808602845" title="Phone Number of Transport Unit Coordinator">
+					<p className="text-lg">Support</p>
+				</Link>
+			</header>
+			<main>
+				{children}
+			</main>
+		</div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
-	<div className="min-h-screen flex flex-col font-[family-name:var(--font-mont)]">
-		<header className="flex flex-row justify-between items-center px-3 py-2">
-			<div className="flex flex-row items-center justify-between gap-2.5">
-				<Image
-					src="/Logo.png"
-					alt="WCIE Logo"
-					width={40}
-					height={40}/>
-				<p className="text-2xl font-[family-name:var(--font-pt-serif)]">WCIE Transport</p>
-			</div>
-			<Link href="tel:+17808602845" title="Phone Number of Transport Unit Coordinator">
-				<p className="text-lg">Support</p>
-			</Link>
-		</header>
-		<main>
-			
-		</main>
+	<div>
+		sdsddsds
+		<p>helel</p>
     </div>
   );
 }
