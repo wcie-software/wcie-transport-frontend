b346bc5c8f78ba4fe4895e3aae9f3580c4da97dc
obaodelana <obaloluwaode@gmail.com>
2025-09-30 14:10:27 -0600

    No need for arguments in cloud environment

--- src/app/utils/firebase.ts
+++ src/app/utils/firebase.ts
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
-let config = (process.env.FIREBASE_WEBAPP_CONFIG)
-	? JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG!)
-	: {
+export const app = (!process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
+	? initializeApp()
+	: initializeApp({
 		apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 		authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 		messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 		appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-	};
-
-export const app = initializeApp(config);
+	});
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
