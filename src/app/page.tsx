2f35f77a70d9dfb0092eaf4b1c30b40cdb25be21
obaodelana <obaloluwaode@gmail.com>
2025-10-18 10:30:59 -0600

    Added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
-
+		<div className="max-w-2xl w-full flex flex-col items-start px-6">
+			<h2 className="font-medium text-3xl md:text-4xl font-[family-name:var(--font-mont)]">
+				Request A Ride To Winners Chapel Int'l Edmonton
+			</h2>
+			<p className="mt-1">It's free.</p>
+			<div className="mt-5 flex flex-row gap-5 items-center justify-between">
+				<Link
+					href="/request"
+					className="bg-primary px-8 py-3 rounded cursor-pointer disabled:bg-gray-500 disabled:cursor-default">
+					Request
+				</Link>
+				<Link href="/admin" className="truncate">Admin Login</Link>
+			</div>
 		</div>
 	);
 }
