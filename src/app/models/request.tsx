3ad8f464bb30068bf41e93fcfd9e3f72a649ffbe
obaodelana <obaloluwaode@gmail.com>
2025-10-21 22:39:00 -0600

    Added request model and crud firestore functions

--- src/app/models/request.tsx
+++ src/app/models/request.tsx
@@ -1,11 +1,12 @@
-export default interface TransportRequest {
-	address: string;
-	fullName: string;
-	googleMapsLink: string;
-	noOfChildren: number;
-	noOfSeats: number;
-	serviceNumber: number;
-	timestamp: Date;
-	phoneNumber: string;
-}
+import * as z from "zod";
 
+export const TransportRequest = z.object({
+	fullName: z.string(),
+	phoneNumber: z.string(),
+	address: z.string(),
+	googleMapsLink: z.url(),
+	serviceNumber: z.int(),
+	noOfChildren: z.nullable(z.int()),
+	noOfSeats: z.int(),
+	timestamp: z.date(),
+});
--- src/app/utils/firestore.ts
+++ src/app/utils/firestore.ts
@@ -1,61 +1,109 @@
-import { doc, getDoc, getDocFromServer, setDoc, updateDoc, query, collection, limit, startAt, getDocs } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc, collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "@/app/utils/firebase";
-import PlaceDetails from "@/app/models/place_details";
-import { User } from "firebase/auth";
-import TransportRequest from "../models/request";
-import { requestConverter } from "./firestore_converter";
+import { ZodObject } from "zod";
 
-const USER_COLLECTION = "users";
-const REQUEST_COLLECTION = "requests";
+enum FirestoreCollections {
+	Users = "users",
+	Requests = "requests",
+}
 
-export async function addUser(user: User): Promise<void> {
-	if (!user.phoneNumber) {
-		// TODO: Throw error
-		return;
-	}
+// export async function addUser(user: User): Promise<void> {
+// 	if (!user.phoneNumber) {
+// 		// TODO: Throw error
+// 		return;
+// 	}
 
-	const userRef = doc(db, USER_COLLECTION, user.uid);
-	const userSnap = await getDoc(userRef);
+// 	const userRef = doc(db, USER_COLLECTION, user.uid);
+// 	const userSnap = await getDoc(userRef);
 
-	if (!userSnap.exists()) {
-		await setDoc(userRef, { "phone_number": user.phoneNumber! });
+// 	if (!userSnap.exists()) {
+// 		await setDoc(userRef, { "phone_number": user.phoneNumber! });
+// 	}
+// }
+
+// export async function getUserLocation(uid: string): Promise<string> {
+// 	const userRef = doc(db, USER_COLLECTION, uid);
+// 	const userSnap = await getDocFromServer(userRef);
+
+// 	if (userSnap.exists()) {
+// 		const data = userSnap.data();
+// 		if ("address" in data) {
+// 			return data["address"] as string;
+// 		}
+// 	}
+
+// 	return "";
+// }
+
+// export async function setUserLocation(
+// 	uid: string, placeDetails: PlaceDetails, address: string
+// ): Promise<void> {
+// 	const userRef = doc(db, USER_COLLECTION, uid);
+// 	await updateDoc(userRef, { address: address, location_details: placeDetails });
+// }
+
+export async function addDocument(
+	collectionName: FirestoreCollections,
+	data: Record<string, any>,
+	documentPath?: string,
+) {
+	if (!documentPath) {
+		await addDoc(collection(db, collectionName), data);
+	} else {
+		const ref = doc(db, collectionName, documentPath);
+		const snapshot = await getDoc(ref);
+		if (!snapshot.exists()) {
+			await setDoc(ref, data);
+		}
 	}
 }
 
-export async function getUserLocation(uid: string): Promise<string> {
-	const userRef = doc(db, USER_COLLECTION, uid);
-	const userSnap = await getDocFromServer(userRef);
+export async function updateDocument(
+	collectionName: FirestoreCollections,
+	documentPath: string,
+	data: Record<string, any>
+): Promise<boolean> {
+	const ref = doc(db, collectionName, documentPath);
+	try {
+		await updateDoc(ref, data);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
 
-	if (userSnap.exists()) {
-		const data = userSnap.data();
-		if ("address" in data) {
-			return data["address"] as string;
+export async function getDocument(
+	collectionName: FirestoreCollections,
+	documentPath: string,
+	schema: ZodObject,
+): Promise<Object | null> {
+	const docRef = doc(db, collectionName, documentPath);
+	const docSnap = await getDoc(docRef);
+
+	if (docSnap.exists()) {
+		const result = schema.safeParse(docSnap.data());
+		if (result.success) {
+			return result.data;
 		}
 	}
 
-	return "";
+	return null;
 }
 
-export async function setUserLocation(
-	uid: string, placeDetails: PlaceDetails, address: string
-): Promise<void> {
-	const userRef = doc(db, USER_COLLECTION, uid);
-	await updateDoc(userRef, { address: address, location_details: placeDetails });
-}
+export async function getCollection(
+	collectionName: FirestoreCollections,
+	schema: ZodObject
+): Promise<Object[]> {
+	const collectionRef = collection(db, collectionName);
+	const snapshot = await getDocs(collectionRef);
 
-export async function getRequests(page: number = 0): Promise<TransportRequest[]> {
-	const requests: TransportRequest[] = [];
-
-	const top = query(
-		collection(db, REQUEST_COLLECTION),
-		limit(10),
-		// startAt(page * 10)
-	).withConverter(requestConverter);
-
-	const snapshot = await getDocs(top);
+	const docs: Object[] = [];
 	snapshot.forEach((doc) => {
-		requests.push(doc.data());
-	})
+		const result = schema.safeParse(doc.data());
+		if (result.success) {
+			docs.push(result.data);
+		}
+	});
 
-	return requests;
+	return docs;
 }
