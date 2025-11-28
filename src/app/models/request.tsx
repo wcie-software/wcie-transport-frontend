8e285b3b1457ac0989bd83d6bc447fc6f02f84fc
obaodelana <obaloluwaode@gmail.com>
2025-11-27 21:14:59 -0700

    Updated requests to also include coordinates

--- src/app/models/request.tsx
+++ src/app/models/request.tsx
@@ -1,11 +1,13 @@
 import * as z from "zod";
 import { BaseDocument } from "@/app/models/base";
+import { Location } from "@/app/models/location";
 
 export const TransportRequestSchema = BaseDocument.extend({
 	full_name: z.string(),
 	phone_number: z.string(),
 	address: z.string(),
 	google_maps_link: z.url(),
+	coordinates: z.optional(Location),
 	service_number: z.coerce.number(),
 	no_of_seats: z.coerce.number(),
 	no_of_children: z.coerce.number().default(0),
