7255baf3a30997b03a867841b9cfe26cc3e6736c
obaodelana <obaloluwaode@gmail.com>
2025-12-01 21:31:53 -0700

    Added validations for schedule page

--- src/app/models/schedule.tsx
+++ src/app/models/schedule.tsx
@@ -2,7 +2,7 @@ import * as z from "zod";
 import { BaseDocument } from "@/app/models/base";
 
 export const ScheduleSchema = BaseDocument.extend({
-	timestamp: z.number().min(1738393200000), // Can't be below 1st Jan. 2025 12:00 AM
+	timestamp: z.number().min(1735714800000), // Can't be below 1st Jan. 2025 12:00 AM
 	schedule: z.record(
 		z.string().min(1).max(2), // Service number: 1, ... 99
 		z.array(z.string()).min(1) // Array of at least length 1
