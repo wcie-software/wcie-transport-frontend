55362f592d53d8640888a2685daa73dc1440b7ae
obaodelana <obaloluwaode@gmail.com>
2025-12-10 17:17:34 -0700

    Added dummy data directly into test apis

--- src/app/models/vehicle.ts
+++ src/app/models/vehicle.ts
@@ -8,14 +8,14 @@ export const VehicleSchema = BaseDocument.extend({
 	active: z.transform((val: string) => {
 		const v = String(val).toLowerCase();
 		return ["true", "yes", "y"].includes(v);
-	}),
+	}).default(true),
 	seating_capacity: z.coerce.number().min(2).default(4),
 	remarks: z.string().default(""),
-	fuel_cost: z.nullable(z.coerce.number().min(0)),
-	last_fuel_date: z.nullable(z.string()),
-	maintenance_type: z.nullable(z.string()),
-	maintenance_receipt_amount: z.nullable(z.coerce.number().min(0)),
-	last_maintenance_date: z.nullable(z.string()),
+	fuel_cost: z.optional(z.coerce.number().min(0)),
+	last_fuel_date: z.optional(z.string()),
+	maintenance_type: z.optional(z.string()),
+	maintenance_receipt_amount: z.optional(z.coerce.number().min(0)),
+	last_maintenance_date: z.optional(z.string()),
 });
 
 export type Vehicle = z.infer<typeof VehicleSchema>;
