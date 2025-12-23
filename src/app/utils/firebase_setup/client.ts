5ab053ceb3ee3fda254e05f8796670339418c44f
obaodelana <obaloluwaode@gmail.com>
2025-12-23 11:51:00 +0200

    Removed unnecessary imports and auto import test data in dev mode

--- src/app/utils/firebase_setup/client.ts
+++ src/app/utils/firebase_setup/client.ts
@@ -9,6 +9,9 @@ export const db = getFirestore(app);
 auth.useDeviceLanguage();
 
 if (process.env.NODE_ENV === "development") {
-	connectAuthEmulator(auth, "http://127.0.0.1:9099");
-	connectFirestoreEmulator(db, "127.0.0.1", 8080);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+
+  // Import test data
+  fetch("/api/imports/import-all", { method: "GET" });
 }
