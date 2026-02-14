462d59026f900a01c9bb3369c2ac1aa214334aaf
obaodelana <obaloluwa.dev@gmail.com>
2026-02-14 15:02:47 -0700

    patch: detect invalid routes

--- src/app/admin/(dashboard)/assignments/page.tsx
+++ src/app/admin/(dashboard)/assignments/page.tsx
@@ -11,114 +11,144 @@ import { Vehicle, VehicleSchema } from "@/app/models/vehicle";
 
 export const dynamic = "force-dynamic";
 
-export default async function AssignmentsPage({ searchParams }: {
-	searchParams: Promise<{ timestamp?: string, service_number?: string }>
+export default async function AssignmentsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ timestamp?: string; service_number?: string }>;
 }) {
-	const params = await searchParams;
+  const params = await searchParams;
 
-	// Attempt to get date from url search param
-	let month, date, year;
-	try {
-		[month, date, year] = params?.timestamp?.split("-").map(parseFloat)!;
-	} catch (e) {
-		// Fallback to today's date
-		const today = new Date();
-		[month, date, year] = [today.getMonth() + 1, today.getDate(), today.getFullYear()];
-	}
+  // Attempt to get date from url search param
+  let month, date, year;
+  try {
+    [month, date, year] = params?.timestamp?.split("-").map(parseFloat)!;
+  } catch (e) {
+    // Fallback to today's date
+    const today = new Date();
+    [month, date, year] = [
+      today.getMonth() + 1,
+      today.getDate(),
+      today.getFullYear(),
+    ];
+  }
 
-	// Construct date manually to mitigate date to string conversion issues
-	const endDate = new Date(year, month - 1, date, 10, 59, 59);
-	// Ensure it falls on a Sunday
-	if (endDate.getDay() !== 0) {
-		endDate.setDate(endDate.getDate() + (7 - endDate.getDay()));
-	}
+  // Construct date manually to mitigate date to string conversion issues
+  const endDate = new Date(year, month - 1, date, 10, 59, 59);
+  // Ensure it falls on a Sunday
+  if (endDate.getDay() !== 0) {
+    endDate.setDate(endDate.getDate() + (7 - endDate.getDay()));
+  }
 
-	const startDate = new Date(endDate);
-	// Start on Monday
-	startDate.setDate(endDate.getDate() - 6);
+  const startDate = new Date(endDate);
+  // Start on Monday
+  startDate.setDate(endDate.getDate() - 6);
 
-	// M-D-YYYY
-	const timestamp = endDate.toLocaleDateString("en-US").replaceAll("/", "-");
+  // M-D-YYYY
+  const timestamp = endDate.toLocaleDateString("en-US").replaceAll("/", "-");
 
-	const service = parseInt(params?.service_number ?? "1");
-	const serviceNumber = isNaN(service) ? 1 : service;
+  const service = parseInt(params?.service_number ?? "1");
+  const serviceNumber = isNaN(service) ? 1 : service;
 
-	const { db } = await getFirebaseAdmin();
-	const fdb = new FirestoreAdminHelper(db);
+  const { db } = await getFirebaseAdmin();
+  const fdb = new FirestoreAdminHelper(db);
 
-	// Get all requests that fall within the time range and are for that service
-	const requestsList = (await fdb.queryCollection<TransportRequest>(
-		FirestoreCollections.Requests,
-		TransportRequestSchema,
-		[
-			{ field: "service_number", operator: "==", value: serviceNumber },
-			{ field: "timestamp", operator: ">=", value: defaultFormatter(startDate) },
-			{ field: "timestamp", operator: "<=", value: defaultFormatter(endDate) },
-		],
-		"timestamp" // Sort by time
-	)).filter((t) => t.status != "cancelled"); // Don't include cancelled
+  // Get all requests that fall within the time range and are for that service
+  const requestsList = (
+    await fdb.queryCollection<TransportRequest>(
+      FirestoreCollections.Requests,
+      TransportRequestSchema,
+      [
+        { field: "service_number", operator: "==", value: serviceNumber },
+        {
+          field: "timestamp",
+          operator: ">=",
+          value: defaultFormatter(startDate),
+        },
+        {
+          field: "timestamp",
+          operator: "<=",
+          value: defaultFormatter(endDate),
+        },
+      ],
+      "timestamp", // Sort by time
+    )
+  ).filter((t) => t.status != "cancelled"); // Don't include cancelled
 
-	let driverList: Driver[] = [];
-	// Get driver schedule for this week
-	const schedule = await fdb.getDocument<Schedule>(
-		FirestoreCollections.Schedules, ScheduleSchema,
-		timestamp
-	);
+  let driverList: Driver[] = [];
+  // Get driver schedule for this week
+  const schedule = await fdb.getDocument<Schedule>(
+    FirestoreCollections.Schedules,
+    ScheduleSchema,
+    timestamp,
+  );
 
-	const serviceNumberString = String(serviceNumber);
-	if (schedule && serviceNumberString in schedule.schedule) {
-		// Get drivers that are in this schedule
-		driverList = await fdb.getDocuments<Driver>(
-			FirestoreCollections.Drivers,
-			DriverSchema,
-			schedule.schedule[serviceNumberString]
-		);
-	}
+  const serviceNumberString = String(serviceNumber);
+  if (schedule && serviceNumberString in schedule.schedule) {
+    // Get drivers that are in this schedule
+    driverList = await fdb.getDocuments<Driver>(
+      FirestoreCollections.Drivers,
+      DriverSchema,
+      schedule.schedule[serviceNumberString],
+    );
+  }
 
-	// Get route for this week
-	const driverRoutes = await fdb.getDocument<DriverRoutes>(
-		FirestoreCollections.Assignments, DriverRoutesSchema,
-		timestamp
-	);
-	// Get only routes for the particular service
-	const routes = driverRoutes?.routes.filter((r) => r.service_number == serviceNumber);
+  // Get route for this week
+  const driverRoutes = await fdb.getDocument<DriverRoutes>(
+    FirestoreCollections.Assignments,
+    DriverRoutesSchema,
+    timestamp,
+  );
+  // Get only routes for the particular service
+  const routes = driverRoutes?.routes.filter(
+    (r) => r.service_number == serviceNumber,
+  );
 
-	// If routes have been generated, get the vehicles assigned to each driver
-	const assignedVehicles: Record<string, Vehicle> = {};
-	if (routes) {
-		const vehicleIdToDriverId: Record<string, string> = {};
-		routes.forEach((r) => {
-			// Vehicle to driver map
-			vehicleIdToDriverId[r.assigned_vehicle_id] = r.driver_id;
-		});
+  let validRoute = true;
+  // If routes have been generated, get the vehicles assigned to each driver
+  const assignedVehicles: Record<string, Vehicle> = {};
+  if (routes) {
+    const vehicleIdToDriverId: Record<string, string> = {};
+    for (const r of routes) {
+      if (!r.assigned_vehicle_id || !r.driver_id) {
+        validRoute = false;
+        break;
+      }
 
-		// Get the vehicles pertaining to the chosen drivers
-		const vehicles = await fdb.getDocuments<Vehicle>(
-			FirestoreCollections.Vehicles,
-			VehicleSchema,
-			Object.keys(vehicleIdToDriverId)
-		);
+      // Vehicle to driver map
+      vehicleIdToDriverId[r.assigned_vehicle_id] = r.driver_id;
+    }
 
-		vehicles.forEach((v) => {
-			try {
-				const driverId = vehicleIdToDriverId[v.documentId!];
-				// Driver to vehicle details map
-				assignedVehicles[driverId] = v;
-			} catch (e) {
-				console.warn(`[Assignments Page] WARNING: Retrieved vehicle '${v.name}' that does not exist in generated route '${driverRoutes?.documentId}'`)
-			}
-		});
-	}
+    if (validRoute) {
+      // Get the vehicles pertaining to the chosen drivers
+      const vehicles = await fdb.getDocuments<Vehicle>(
+        FirestoreCollections.Vehicles,
+        VehicleSchema,
+        Object.keys(vehicleIdToDriverId),
+      );
 
-	return (
-		<div className="w-full h-screen">
-			<AssignmentsView
-				timestamp={timestamp}
-				driversList={driverList}
-				assignedVehicles={assignedVehicles}
-				requestsList={requestsList}
-				routes={routes}
-			/>
-		</div>
-	);
+      vehicles.forEach((v) => {
+        try {
+          const driverId = vehicleIdToDriverId[v.documentId!];
+          // Driver to vehicle details map
+          assignedVehicles[driverId] = v;
+        } catch (e) {
+          console.warn(
+            `[Assignments Page] WARNING: Retrieved vehicle '${v.name}' that does not exist in generated route '${driverRoutes?.documentId}'`,
+          );
+        }
+      });
+    }
+  }
+
+  return (
+    <div className="w-full h-screen">
+      <AssignmentsView
+        timestamp={timestamp}
+        driversList={driverList}
+        assignedVehicles={assignedVehicles}
+        requestsList={requestsList}
+        routes={validRoute ? routes : undefined}
+      />
+    </div>
+  );
 }
--- src/app/utils/firestore_admin.ts
+++ src/app/utils/firestore_admin.ts
@@ -1,4 +1,9 @@
-import { Firestore, Query, WhereFilterOp, FieldPath } from "firebase-admin/firestore";
+import {
+  Firestore,
+  Query,
+  WhereFilterOp,
+  FieldPath,
+} from "firebase-admin/firestore";
 import { FirestoreCollections } from "./firestore";
 import { ZodType } from "zod";
 
@@ -7,172 +12,183 @@ import { ZodType } from "zod";
  * Provides typed access to Firestore collections and documents with Zod schema validation.
  */
 export class FirestoreAdminHelper {
-	_db: Firestore;
+  _db: Firestore;
 
-	constructor(db: Firestore) {
-		this._db = db;
-	}
+  constructor(db: Firestore) {
+    this._db = db;
+  }
 
-	/**
-	 * Get all documents in `collectionName`.
-	 * Note: If a document does not conform to `schema`, it is skipped.
-	 * @param collectionName Name of collection
-	 * @param schema Table schema
-	 * @param orderByField Field to order the results by (optional)
-	 * @param orderByDirection Direction to order the results ("asc" or "desc"). Defaults to descending order
-	 * @returns List of documents in the collection
-	 */
-	async getCollection<Type>(
-		collectionName: FirestoreCollections,
-		schema: ZodType<Type>,
-		orderByField?: string,
-		orderByDirection: "asc" | "desc" = "desc",
-	): Promise<Type[]> {
-		const collection = this._db.collection(collectionName);
+  /**
+   * Get all documents in `collectionName`.
+   * Note: If a document does not conform to `schema`, it is skipped.
+   * @param collectionName Name of collection
+   * @param schema Table schema
+   * @param orderByField Field to order the results by (optional)
+   * @param orderByDirection Direction to order the results ("asc" or "desc"). Defaults to descending order
+   * @returns List of documents in the collection
+   */
+  async getCollection<Type>(
+    collectionName: FirestoreCollections,
+    schema: ZodType<Type>,
+    orderByField?: string,
+    orderByDirection: "asc" | "desc" = "desc",
+  ): Promise<Type[]> {
+    const collection = this._db.collection(collectionName);
 
-		const results = await (orderByField
-			// Sort documents by `orderField` in `orderDirection
-			? collection.orderBy(orderByField, orderByDirection)
-			: collection
-		).get();
+    const results = await (
+      orderByField
+        ? // Sort documents by `orderField` in `orderDirection
+          collection.orderBy(orderByField, orderByDirection)
+        : collection
+    ).get();
 
-		if (!results.empty) {
-			const list: Type[] = [];
-			results.forEach(doc => {
-				const requestData = doc.data();
-				// Set documentId to the document's id
-				// See `models/base.tsx` for reason
-				requestData.documentId = doc.id;
+    if (!results.empty) {
+      const list: Type[] = [];
+      results.forEach((doc) => {
+        const requestData = doc.data();
+        // Set documentId to the document's id
+        // See `models/base.tsx` for reason
+        requestData.documentId = doc.id;
 
-				const parsed = schema.safeParse(requestData);
-				if (parsed.success) {
-					list.push(parsed.data as Type);
-				} else {
-					console.warn(`Failed to parse '${collectionName}/${doc.id}' data:`, parsed.error);
-				}
-			});
+        const parsed = schema.safeParse(requestData);
+        if (parsed.success) {
+          list.push(parsed.data as Type);
+        } else {
+          console.warn(
+            `Failed to parse '${collectionName}/${doc.id}' data:`,
+            parsed.error,
+          );
+        }
+      });
 
-			return list;
-		}
+      return list;
+    }
 
-		return [];
-	}
+    return [];
+  }
 
-	/**
-	 * Get specified documents from `collectionName`
-	 * @param collectionName Name of the collection
-	 * @param schema Table schema
-	 * @param documentIds List of documents to retrieve
-	 * @param orderByField Optional field to order the results by
-	 * @param orderByDirection Direction to order the results ("asc" or "desc"). Defaults to descending order
-	 * @returns List of documents that exist and conform to `schema`
-	 */
-	async getDocuments<Type>(
-		collectionName: FirestoreCollections,
-		schema: ZodType<Type>,
-		documentIds: string[],
-		orderByField?: string,
-		orderByDirection: "asc" | "desc" = "desc",
-	): Promise<Type[]> {
-		if (documentIds.length === 0) {
-			return [];
-		}
+  /**
+   * Get specified documents from `collectionName`
+   * @param collectionName Name of the collection
+   * @param schema Table schema
+   * @param documentIds List of documents to retrieve
+   * @param orderByField Optional field to order the results by
+   * @param orderByDirection Direction to order the results ("asc" or "desc"). Defaults to descending order
+   * @returns List of documents that exist and conform to `schema`
+   */
+  async getDocuments<Type>(
+    collectionName: FirestoreCollections,
+    schema: ZodType<Type>,
+    documentIds: string[],
+    orderByField?: string,
+    orderByDirection: "asc" | "desc" = "desc",
+  ): Promise<Type[]> {
+    if (documentIds.length === 0) {
+      return [];
+    }
+    const collection = this._db.collection(collectionName);
+    let query = collection.where(FieldPath.documentId(), "in", documentIds);
+    if (orderByField) {
+      query = query.orderBy(orderByField, orderByDirection);
+    }
 
-		const collection = this._db.collection(collectionName);
-		let query = collection.where(FieldPath.documentId(), "in", documentIds);
-		if (orderByField) {
-			query = query.orderBy(orderByField, orderByDirection);
-		}
+    const results = await query.get();
+    if (!results.empty) {
+      const list: Type[] = [];
+      results.forEach((doc) => {
+        const requestData = doc.data();
+        // Set documentId to the document's id
+        // See `models/base.tsx` for reason
+        requestData.documentId = doc.id;
 
-		const results = await query.get();
-		if (!results.empty) {
-			const list: Type[] = [];
-			results.forEach(doc => {
-				const requestData = doc.data();
-				// Set documentId to the document's id
-				// See `models/base.tsx` for reason
-				requestData.documentId = doc.id;
+        const parsed = schema.safeParse(requestData);
+        if (parsed.success) {
+          list.push(parsed.data as Type);
+        } else {
+          console.warn(
+            `Failed to parse '${collectionName}/${doc.id}' data:`,
+            parsed.error,
+          );
+        }
+      });
 
-				const parsed = schema.safeParse(requestData);
-				if (parsed.success) {
-					list.push(parsed.data as Type);
-				} else {
-					console.warn(`Failed to parse '${collectionName}/${doc.id}' data:`, parsed.error);
-				}
-			});
+      return list;
+    }
 
-			return list;
-		}
+    return [];
+  }
 
-		return [];
-	}
+  /**
+   * Get a single document from `collectionName` by its name.
+   * Underneath it passes a single document name to {@link getDocuments}.
+   * @param collectionName Name of the collection
+   * @param schema Table schema
+   * @param documentId ID of the document to retrieve
+   * @returns The document if it exists and conforms to `schema`, undefined otherwise
+   */
+  async getDocument<Type>(
+    collectionName: FirestoreCollections,
+    schema: ZodType<Type>,
+    documentId: string,
+  ): Promise<Type | undefined> {
+    const doc = await this.getDocuments<Type>(collectionName, schema, [
+      documentId,
+    ]);
+    if (doc.length > 0) {
+      return doc[0];
+    } else {
+      return undefined;
+    }
+  }
 
-	/**
-	 * Get a single document from `collectionName` by its name.
-	 * Underneath it passes a single document name to {@link getDocuments}.
-	 * @param collectionName Name of the collection
-	 * @param schema Table schema
-	 * @param documentId ID of the document to retrieve
-	 * @returns The document if it exists and conforms to `schema`, undefined otherwise
-	 */
-	async getDocument<Type>(
-		collectionName: FirestoreCollections,
-		schema: ZodType<Type>,
-		documentId: string
-	): Promise<Type | undefined> {
-		const doc = await this.getDocuments<Type>(collectionName, schema, [documentId]);
-		if (doc.length > 0) {
-			return doc[0];
-		} else {
-			return undefined;
-		}
-	}
+  /**
+   * Queries a collection with multiple where clauses.
+   * @param collectionName Name of the collection
+   * @param schema Table schema
+   * @param whereClauses List of where clauses to AND together
+   * @param orderByField Optional field to order the results by
+   * @param orderByDirection Direction to order the results ("asc" or "desc"). Defaults to descending order
+   * @returns List of documents matching the query that conform to `schema`
+   */
+  async queryCollection<Type>(
+    collectionName: FirestoreCollections,
+    schema: ZodType<Type>,
+    whereClauses: { field: string; operator: WhereFilterOp; value: any }[] = [],
+    orderByField?: string,
+    orderByDirection: "asc" | "desc" = "desc",
+  ): Promise<Type[]> {
+    let query: Query = this._db.collection(collectionName);
+    // And clauses together
+    for (const clause of whereClauses) {
+      query = query.where(clause.field, clause.operator, clause.value);
+    }
 
-	/**
-	 * Queries a collection with multiple where clauses.
-	 * @param collectionName Name of the collection
-	 * @param schema Table schema
-	 * @param whereClauses List of where clauses to AND together
-	 * @param orderByField Optional field to order the results by
-	 * @param orderByDirection Direction to order the results ("asc" or "desc"). Defaults to descending order
-	 * @returns List of documents matching the query that conform to `schema`
-	 */
-	async queryCollection<Type>(
-		collectionName: FirestoreCollections,
-		schema: ZodType<Type>,
-		whereClauses: { field: string, operator: WhereFilterOp, value: any }[] = [],
-		orderByField?: string,
-		orderByDirection: "asc" | "desc" = "desc",
-	): Promise<Type[]> {
-		let query: Query = this._db.collection(collectionName);
-		// And clauses together
-		for (const clause of whereClauses) {
-			query = query.where(clause.field, clause.operator, clause.value);
-		}
+    // Sort results by `orderField`
+    if (orderByField) {
+      query = query.orderBy(orderByField, orderByDirection);
+    }
 
-		// Sort results by `orderField`
-		if (orderByField) {
-			query = query.orderBy(orderByField, orderByDirection);
-		}
+    const results = await query.get();
+    if (!results.empty) {
+      const list: Type[] = [];
+      results.forEach((doc) => {
+        const requestData = doc.data();
+        requestData.documentId = doc.id;
 
-		const results = await query.get();
-		if (!results.empty) {
-			const list: Type[] = [];
-			results.forEach(doc => {
-				const requestData = doc.data();
-				requestData.documentId = doc.id;
+        const parsed = schema.safeParse(requestData);
+        if (parsed.success) {
+          list.push(parsed.data as Type);
+        } else {
+          console.warn(
+            `Failed to parse '${collectionName}/${doc.id}' data:`,
+            parsed.error,
+          );
+        }
+      });
 
-				const parsed = schema.safeParse(requestData);
-				if (parsed.success) {
-					list.push(parsed.data as Type);
-				} else {
-					console.warn(`Failed to parse '${collectionName}/${doc.id}' data:`, parsed.error);
-				}
-			});
+      return list;
+    }
 
-			return list;
-		}
-
-		return [];
-	}
+    return [];
+  }
 }
